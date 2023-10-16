882207397f71665a4bdf1a8e8a3ff71e8a700817
yxy <yxy360@gmail.com>
2023-10-16 00:52:09 -0400

    navigation bar part 1

--- application/src/main/kotlin/Main.kt
+++ application/src/main/kotlin/Main.kt
@@ -22,8 +22,8 @@ import navcontroller.NavController
 import navcontroller.NavigationHost
 import navcontroller.composable
 import navcontroller.rememberNavController
-import screens.WelcomePage
 import screens.SchedulePage
+import screens.welcomePage
 import style.AppTheme
 
 fun main() = application {
@@ -61,7 +61,7 @@ fun CustomNavigationHost(
 ) {
     NavigationHost(navController) {
         composable(Screen.WelcomePage.name) {
-            WelcomePage(navController)
+            welcomePage(navController)
         }
 
         composable(Screen.SchedulePage.name) {
