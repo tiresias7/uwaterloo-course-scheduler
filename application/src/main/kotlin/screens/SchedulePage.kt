02c1cfbc557a2630b57058deb23ee60868a0c1a0
yxy <yxy360@gmail.com>
2023-10-27 22:38:58 -0400

    rebase to main and clean up

--- application/src/main/kotlin/screens/SchedulePage.kt
+++ application/src/main/kotlin/screens/SchedulePage.kt
@@ -1,27 +1,17 @@
 package screens
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.ExperimentalMaterialApi
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
-import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
 import androidx.compose.ui.window.DialogProperties
-import components.*
 import components.courseSelectionSection
 import components.navDrawer
 import components.preferenceSelectionSection
@@ -29,7 +19,6 @@ import components.scheduleSection
 import createDataSource
 import data.SectionUnit
 import data.SelectedCourse
-import kotlinx.serialization.json.*
 import navcontroller.NavController
 import queryAllClasses
 import testAlgo
@@ -162,13 +151,15 @@ fun preferenceSelectionWrapper(
     clicked: MutableState<Boolean>,
     showCallBack: () -> Unit
 ) {
-    val preferences = remember { mutableStateListOf(
-        "Time for classes", "Time for breaks",
-        "Time of conflicts", "Location", "Instructor",
-    )}
+    val preferences = remember {
+        mutableStateListOf(
+            "Time for classes", "Time for breaks",
+            "Time of conflicts", "Location", "Instructor",
+        )
+    }
     //var addedPreferences = remember { mutableStateListOf<Preference>() }  // state hoisting
     preferenceSelectionSection(preferences, showCallBack,
-        changeCallBack = { from : Int , to : Int ->
+        changeCallBack = { from: Int, to: Int ->
             val removeElement = preferences.removeAt(from)
             preferences.add(to, removeElement)
         })
