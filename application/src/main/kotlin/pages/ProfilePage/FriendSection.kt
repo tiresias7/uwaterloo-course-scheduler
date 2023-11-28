7ea33f209cc1bc3f947fad5b336b2bca9a96e744
Alex Lin <a68lin@uwaterloo.ca>
2023-11-28 11:34:26 -0500

    Add friend API

--- application/src/main/kotlin/pages/ProfilePage/FriendSection.kt
+++ application/src/main/kotlin/pages/ProfilePage/FriendSection.kt
@@ -9,6 +9,7 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color.Companion.White
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import friendSearchInputField
@@ -43,6 +44,7 @@ var allUser : MutableList<Pair<Int, String>> = mutableListOf()
 @Composable
 fun friendSection( USER_ID : Int) {
     val friendsList = remember { mutableStateOf(fetchFriendList(USER_ID)) }
+    //val friendRequestList = remember { mutableListOf<Pair<Int, String>>() }
     val friendRequestList = remember { mutableStateOf(fetchFriendRequests(USER_ID)) }
     var ifFriendList by remember { mutableStateOf(true)}
     var ifMessages by remember { mutableStateOf(false)}
@@ -64,7 +66,9 @@ fun friendSection( USER_ID : Int) {
                 "ananana"
             )
         }*/
-    Column {
+    Column (
+    )
+    {
         Text(text = "My Friends", fontSize = 30.sp)
         Row(
             modifier = Modifier.padding(top = 10.dp),
@@ -73,47 +77,51 @@ fun friendSection( USER_ID : Int) {
         ) {
             if (ifFriendList) {
                 OutlinedButton(
-                    content = { Text("Friend List") },
+                    content = { Text("View and Add") },
                     onClick = {
+                        friendsList.value = fetchFriendList(USER_ID)
                         ifFriendList = true
                         ifMessages = false
                         ifAddFriends = false
                     },
-                    modifier = Modifier.size(130.dp, 40.dp)
+                    modifier = Modifier.size(180.dp, 40.dp)
                 )
             } else {
                 TextButton(
-                    content = { Text("Friend List") },
+                    content = { Text("View and Add") },
                     onClick = {
+                        friendsList.value = fetchFriendList(USER_ID)
                         ifFriendList = true
                         ifMessages = false
                         ifAddFriends = false
                     },
-                    modifier = Modifier.size(130.dp, 40.dp)
+                    modifier = Modifier.size(180.dp, 40.dp)
                 )
             }
             if (ifMessages) {
                 OutlinedButton(
-                    content = { Text("Messages") },
+                    content = { Text("Friend Requests") },
                     onClick = {
+                        friendRequestList.value = fetchFriendRequests(USER_ID)
                         ifFriendList = false
                         ifMessages = true
                         ifAddFriends = false
                     },
-                    modifier = Modifier.size(130.dp, 40.dp)
+                    modifier = Modifier.size(180.dp, 40.dp).padding(start = 5.dp)
                 )
             } else {
                 TextButton(
-                    content = { Text("Messages") },
+                    content = { Text("Friend Requests") },
                     onClick = {
+                        friendRequestList.value = fetchFriendRequests(USER_ID)
                         ifFriendList = false
                         ifMessages = true
                         ifAddFriends = false
                     },
-                    modifier = Modifier.size(130.dp, 40.dp)
+                    modifier = Modifier.size(180.dp, 40.dp).padding(start = 5.dp)
                 )
             }
-            if (ifAddFriends) {
+            /*if (ifAddFriends) {
                 OutlinedButton(
                     content = { Text("Add Friends") },
                     onClick = {
@@ -133,44 +141,70 @@ fun friendSection( USER_ID : Int) {
                     },
                     modifier = Modifier.size(140.dp, 40.dp)
                 )
-            }
+            }*/
         }
         Box(
             modifier = Modifier.height(650.dp).fillMaxWidth().padding(top = 10.dp),
         ) {
             if (ifFriendList) {
-                ElevatedCard(
-                    elevation = CardDefaults.cardElevation(
-                        defaultElevation = 6.dp
-                    ),
-                    modifier = Modifier,
+                Column(
+                    verticalArrangement = Arrangement.Top
                 ) {
-                    LazyColumn(
+                    Text(
+                        "Add Friends:",
                         modifier = Modifier,
+                        fontSize = 12.sp,
+                        fontStyle = FontStyle.Italic
+                    )
+                    friendSearchInputField(allUser, {}, USER_ID)
+                    Text(
+                        "My Friend List:",
+                        modifier = Modifier.padding(top = 10.dp, bottom = 5.dp),
+                        fontSize = 12.sp,
+                        fontStyle = FontStyle.Italic
+                    )
+                    ElevatedCard(
+                        elevation = CardDefaults.cardElevation(
+                            defaultElevation = 6.dp
+                        ),
                     ) {
-                        items(friendsList.value, { it }) { friend ->
-                            ListItem(
-                                modifier = Modifier
-                                    .height(70.dp).fillMaxWidth(),
-                                headlineContent = { Text(friend.second) },
-                                trailingContent = {
-                                     Row() {
-                                         OutlinedButton(
-                                             onClick = { fetchFriendProfile(USER_ID, friend.first) },
-                                             modifier = Modifier.padding(5.dp)
-                                         ) {
-                                             Text("View profile")
-                                         }
-                                     }
-                                },
-                                tonalElevation = 30.dp,
-                                colors = ListItemDefaults.colors(containerColor = md_theme_light_primaryContainer)
-                            )
-                            Divider(thickness = 1.dp, color = md_theme_light_inversePrimary)
+                        LazyColumn(
+                            modifier = Modifier,
+                        ) {
+                            items(friendsList.value, { it }) { friend ->
+                                ListItem(
+                                    modifier = Modifier
+                                        .height(50.dp).fillMaxWidth(),
+                                    headlineContent = { Text(friend.second) },
+                                    trailingContent = {
+                                        Row() {
+                                            TextButton(
+                                                onClick = { fetchFriendProfile(USER_ID, friend.first) },
+                                                modifier = Modifier.padding(end = 4.dp)
+                                            ) {
+                                                Text("View profile")
+                                            }
+                                            TextButton(
+                                                onClick = { },
+                                                modifier = Modifier
+                                            ) {
+                                                Text("Remove")
+                                            }
+                                        }
+                                    },
+                                    tonalElevation = 30.dp,
+                                    colors = ListItemDefaults.colors(containerColor = md_theme_light_primaryContainer)
+                                )
+                                Divider(thickness = 1.dp, color = md_theme_light_inversePrimary)
+                            }
                         }
                     }
                 }
             } else if (ifMessages) {
+                if (friendRequestList.value.isEmpty()) {
+                    Text("You currently have no invitation",
+                            fontStyle = FontStyle.Italic)
+                }
                 ElevatedCard(
                     elevation = CardDefaults.cardElevation(
                         defaultElevation = 6.dp
@@ -190,7 +224,12 @@ fun friendSection( USER_ID : Int) {
 
                                     ) {
                                         OutlinedButton(
-                                            onClick = { approveFriendRequest(USER_ID, user.first) },
+                                            onClick = {
+                                                approveFriendRequest(USER_ID, user.first)
+                                                //friendRequestList.clear()
+                                                //friendRequestList.addAll(fetchFriendRequests(USER_ID))
+                                                friendRequestList.value = fetchFriendRequests(USER_ID)
+                                            },
                                             modifier = Modifier.padding(5.dp)
                                         ) {
                                             Text("Accept")
@@ -211,7 +250,7 @@ fun friendSection( USER_ID : Int) {
                     }
                 }
             } else if (ifAddFriends) {
-                friendSearchInputField(allUser, {})
+                friendSearchInputField(allUser, {}, USER_ID)
             }
         }
     }
