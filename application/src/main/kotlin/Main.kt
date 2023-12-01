58de2f1748256c1b7cc3d067c1d590198a51c196
yxy <yxy360@gmail.com>
2023-12-01 01:05:55 -0500

    save user preferences to local file

--- application/src/main/kotlin/Main.kt
+++ application/src/main/kotlin/Main.kt
@@ -2,30 +2,96 @@ import pages.ProfilePage.profilePage
 import pages.SchedulePage.schedulePage
 import pages.WelcomePage.welcomePage
 import androidx.compose.runtime.*
-import androidx.compose.ui.window.Window
-import androidx.compose.ui.window.WindowPlacement
-import androidx.compose.ui.window.application
-import androidx.compose.ui.window.rememberWindowState
+import androidx.compose.ui.unit.DpSize
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.*
 import common.navcontroller.NavController
 import common.navcontroller.NavigationHost
 import common.navcontroller.composable
 import common.navcontroller.rememberNavController
 import pages.LoginPage.loginPage
 import style.AppTheme
+import style.MyColorTheme
+import style.currentColorScheme
+import java.io.FileInputStream
+import java.io.FileOutputStream
+import java.util.*
 
 fun main() = application {
-    val windowState = rememberWindowState(placement = WindowPlacement.Fullscreen)
+    val savedPreferences = readFromFile()
+    val windowState = savedPreferences.second ?: rememberWindowState(placement = WindowPlacement.Fullscreen)
+    if (savedPreferences.first != null){
+        currentColorScheme.value = savedPreferences.first!!
+    }
+
     Window(
         state = windowState,
         title = "UW Course Scheduler",
-        onCloseRequest = ::exitApplication
+        onCloseRequest = {
+            onCloseSave(windowState, currentColorScheme.value)
+            exitApplication()
+        }
     ) {
         AppTheme { App() }
     }
 }
 
+fun readFromFile(): Pair<MyColorTheme?, WindowState?> {
+    val properties = Properties()
+    var cs: MyColorTheme?
+
+    try {
+        FileInputStream("local.properties").use { inputStream ->
+            properties.load(inputStream)
+            try {
+                cs = MyColorTheme.valueOf(properties.getProperty("theme") ?: "null")
+            } catch (e: IllegalArgumentException) {
+                cs = null
+            }
+
+            val x = properties.getProperty("window.x")?.toFloatOrNull() ?: return Pair(cs, null)
+            val y = properties.getProperty("window.y")?.toFloatOrNull() ?: return Pair(cs, null)
+            val width = properties.getProperty("window.width")?.toFloatOrNull() ?: return Pair(cs, null)
+            val height = properties.getProperty("window.height")?.toFloatOrNull() ?: return Pair(cs, null)
+
+            return Pair(
+                cs, WindowState(
+                    position = WindowPosition(x.dp, y.dp),
+                    size = DpSize(width.dp, height.dp)
+                )
+            )
+        }
+    } catch (e: Exception) {
+        // Handle file not found or invalid properties
+        return Pair(null, null)
+    }
+}
+
+fun onCloseSave(
+    windowState: WindowState,
+    cs: MyColorTheme
+) {
+    val properties = Properties()
+    val position = windowState.position
+    val size = windowState.size
+
+    properties.setProperty("window.x", position.x.value.toString())
+    properties.setProperty("window.y", position.y.value.toString())
+    properties.setProperty("window.width", size.width.value.toString())
+    properties.setProperty("window.height", size.height.value.toString())
+    properties.setProperty("theme", cs.name)
+
+    try {
+        FileOutputStream("local.properties").use { outputStream ->
+            properties.store(outputStream, "Saved preference")
+        }
+    } catch (e: Exception) {
+        e.printStackTrace()
+    }
+}
+
 @Composable
-fun App(){
+fun App() {
     val navController by rememberNavController(Screen.LoginPage.name)
     CustomNavigationHost(navController = navController)
 }
