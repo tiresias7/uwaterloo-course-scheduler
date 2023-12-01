b43d53806e34c97072a15ab9fe70d1bcd9280163
Jason Ye <yzm0614@gmail.com>
2023-11-30 20:20:31 -0500

    add username when sign in

--- server/src/main/kotlin/com/example/logic/user.kt
+++ server/src/main/kotlin/com/example/logic/user.kt
@@ -6,24 +6,25 @@ import com.example.database.common.createDataSource
 import com.example.database.users.createUser
 import com.example.database.users.verifyPasswordByUID
 
-fun signUpNewUsers(name: String, passwordHashed: String, email: String): Pair<SignStatus, Int> {
+fun signUpNewUsers(name: String, passwordHashed: String, email: String): Pair<SignStatus, Pair<Int, String>> {
     createDataSource().use { db ->
-        val uid = queryUIDByEmail(email, db)
-        val status = if (uid == 0) SignStatus.SIGN_UP_CREATE else SignStatus.SIGN_UP_FAILED
+        var uidNamePair = queryUIDByEmail(email, db)
+        val status = if (uidNamePair.first == 0) SignStatus.SIGN_UP_CREATE else SignStatus.SIGN_UP_FAILED
         if (status == SignStatus.SIGN_UP_CREATE) {
             createUser(name, passwordHashed, email, db)
+            uidNamePair = queryUIDByEmail(email, db)
         }
-        return Pair(status, uid)
+        return Pair(status, uidNamePair)
     }
 }
 
-fun signInExistingUsersByEmail(email: String, password: String): Pair<SignStatus, Int> {
+fun signInExistingUsersByEmail(email: String, password: String): Pair<SignStatus, Pair<Int, String>> {
     createDataSource().use { db ->
-        val uid = queryUIDByEmail(email, db)
-        if (uid == 0) return Pair(SignStatus.SIGN_IN_INVALID, uid)
-        if (!verifyPasswordByUID(uid, password, db)) {
-            return Pair(SignStatus.SIGN_IN_FAILED, uid)
+        val uidNamePair = queryUIDByEmail(email, db)
+        if (uidNamePair.first == 0) return Pair(SignStatus.SIGN_IN_INVALID, uidNamePair)
+        if (!verifyPasswordByUID(uidNamePair.first, password, db)) {
+            return Pair(SignStatus.SIGN_IN_FAILED, uidNamePair)
         }
-        return Pair(SignStatus.SIGN_IN_SUCCESS, uid)
+        return Pair(SignStatus.SIGN_IN_SUCCESS, uidNamePair)
     }
 }
