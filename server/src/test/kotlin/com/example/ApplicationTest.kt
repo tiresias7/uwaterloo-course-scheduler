b0561d103d8e0c857ab06424b38bb5bc0aeefc4b
yxy <yxy360@gmail.com>
2023-12-05 15:34:14 -0500

    test the service is online

--- server/src/test/kotlin/com/example/ApplicationTest.kt
+++ server/src/test/kotlin/com/example/ApplicationTest.kt
@@ -1,14 +1,23 @@
 package com.example
 
-import com.example.plugins.*
+import com.example.plugins.configureRouting
+import io.ktor.http.*
 import io.ktor.server.testing.*
-import kotlin.test.*
+import io.ktor.util.KtorExperimentalAPI
+import org.junit.Test
+import kotlin.test.assertEquals
 
+@KtorExperimentalAPI
 class ApplicationTest {
+
     @Test
-    fun testRoot() = testApplication {
-        application {
-            configureRouting()
+    fun testRoot() {
+        withTestApplication({ configureRouting() }) {
+            handleRequest(HttpMethod.Get, "/").apply {
+                assertEquals(HttpStatusCode.OK, response.status())
+                assertEquals("Hello World!", response.content)
+            }
         }
     }
 }
+
